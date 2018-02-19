0b155e6269d4fcaabef1dbc1cc3eba86038caf7c
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-19 13:05:29 -0600

    API working. Now to display results.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Wikipedia Viewer
 
+To do
 1.Check wikipedia for search results.
 2.Convert wikipedia JSON string to object variable to work with.
---3.Create divs where search results will go for display.
+
 4.Allow search to be completed again.
 --5.Style divisions.
+
+
+Basically done
+3.Create divs where search results will go for display.
--- script.js
+++ script.js
@@ -9,37 +9,61 @@ $(document).ready(() => {
       if(!this.centerRandomRemoved){
         $('.contain3').addClass("animated fadeOut")
 
-        setTimeout(() => {
-          $('h2').append('<span style="font-size: 65%"><a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank"> (random)</a></span>')
+        setTimeout( () => {
+          $('h2').append('<span style="font-size: 65%"><a href="https://en.wikipedia.org/wiki/Special:Random"' +
+                          'target="_blank"> (random)</a></span>')
         },1000)       
         this.centerRandomRemoved = true
       }
     },
+    
+    search: () => {
+      $.ajax( {
+        url: 'https://en.wikipedia.org/w/api.php?',
+        data: 'action=query&list=search&srsearch=' + entry.searchFor + 
+                '&prop=revisions&rvprop=content&format=json&formatversion=2',
+        dataType: 'jsonp',
+        type: 'GET',
+        headers: { 'Api-User-Agent': 'Example/1.0' },
+        success: function(data) {
+             //alert(data.query.search[0].title + '<br/><br/>' + data.query.search[0].snippet)
+             this.searchResults = data
+        }
+    } )
+      .fail( function() {
+        alert('No workie.')
+      })
+      .always( function() {
+        console.log('Done')
+      });
+    },
 
     showSearch: () => {
-         setTimeout(() => {
-           $('.contain3').html('')
-           $('.contain3').removeClass("animated fadeOut")
-           $('.contain3').css("padding","20px 0px 0px 30px")
-           $('.contain3').html('<div class="col wikiResults"></div>') 
-           
-           for(let i = 0; i < 10; i++){
-                $('.wikiResults').append('<div class="eachResult"><span class="title">' + 
-                    'Title</span><span class="desc"> : Wiki information for ' +
-                    'one of the search results.</span></div>')
-           }
-        },1000)
+      setTimeout( () => {
+        $('.contain3').html('')
+        $('.contain3').removeClass("animated fadeOut")
+        $('.contain3').css("padding","20px 0px 0px 30px")
+        $('.contain3').html('<div class="col wikiResults"></div>') 
+        
+        for(let i = 0; i < 10; i++){
+          debugger
+            $('.wikiResults').append('<div class="eachResult"><span class="title">' + 
+                this.searchResults.query.search[i].title + '</span><span class="desc">' +
+                + this.searchResults.query.search[i].snippet +'</span></div>')
+                
+        }
+    },1000)
   }
 }                              
         // Section handles Enter keypress and Search button click
         $('#entry').keypress( x => {
-            if(x.charCode == 13 && $('#entry').val()){
+            if ( x.charCode == 13 && $('#entry').val() ) {
                 keyEnterSubmit()
             }
         })
         
         $('#submit').on('click', () => {
-            if($('#entry').val()){
+            if ( $('#entry').val() ) {
                 keyEnterSubmit()
             }
         })
@@ -49,7 +73,7 @@ $(document).ready(() => {
          // if(entry.searchFor != $('#wikiEntry').val()){
             entry.searchFor = $('#entry').val()
             entry.removeRandom()
-            //entry.search()
+            entry.search()
             entry.showSearch()
          // }
           
@@ -95,3 +119,15 @@ $(document).ready(function(){
 });
 */
 
+// let exampleData = {
+//  "batchcomplete":true,
+//  "continue":{"sroffset":10,"continue":"-||revisions"},
+//  "query":{"searchinfo":{"totalhits":224474},
+//          "search":[{"ns":0,"title":"Car","pageid":13673345,"size":67051,"wordcount":7245,
+//             "snippet":"A <span class=\"searchmatch\">car</span> (or automobile) is" +
+//             "a wheeled motor vehicle used for transportation. Most definitions of" +
+//             "<span class=\"searchmatch\">car</span> say they run primarily on roads," + 
+//             "seat one to eight people","timestamp":"2018-02-11T18:23:41Z"},
+//      {"ns":0,"title":"Central African Republic","pageid":5478,"size":71631,"wordcount":7018,"snippet":"Coordinates: 7°N 21°E﻿ / ﻿7°N 21°E﻿ / 7; 21 The Central African Republic (<span class=\"searchmatch\">CAR</span>; Sango: Ködörösêse tî Bêafrîka; French: République centrafricaine  pronounced [ʁepyblik","timestamp":"2018-02-15T22:37:43Z"},{"ns":0,"title":"The Car","pageid":2938431,"size":11103,"wordcount":1360,"snippet":"The <span class=\"searchmatch\">Car</span> is a 1977 American thriller film directed by Elliot Silverstein and written by Michael Butler, Dennis Shryack and Lane Slate. The film stars James","timestamp":"2018-02-16T01:53:56Z"},{"ns":0,"title":"Car classification","pageid":381336,"size":53002,"wordcount":5408,"snippet":"Governments and private organizations have developed <span class=\"searchmatch\">car</span> classification schemes that are used for innumerable purposes including regulation, description","timestamp":"2018-02-09T03:49:27Z"},{"ns":0,"title":"Elon Musk's Tesla Roadster","pageid":55947330,"size":47446,"wordcount":3932,"snippet":"electric sports <span class=\"searchmatch\">car</span> that served as the dummy payload for the Falcon Heavy test flight on February 6,"}]
+// }
+// }
